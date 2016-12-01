6de069338f3e246a233d33ddec4d276c4bef58ac
Levi Burner <leviburner@gmail.com>
2016-12-01 00:09:16 -0500

    Rewrite for interpolation

--- src/AccelerationPlanner.hpp
+++ src/AccelerationPlanner.hpp
@@ -16,6 +16,8 @@
 #include "geometry_msgs/TwistStamped.h"
 #include "QuadVelocityController.hpp"
 
+using geometry_msgs::TwistStamped;
+
 namespace Iarc7Motion
 {
     template<class T>
@@ -39,6 +41,10 @@ namespace Iarc7Motion
         void processVelocityCommand(const iarc7_msgs::TwistStampedArrayStamped::ConstPtr& message);
 
         void dispatchVelocity(const ros::TimerEvent&);
+
+        void trimVelocityQueue(ros::Time time);
+
+        static TwistStamped interpolateTwists(TwistStamped& begin, TwistStamped& end, ros::Time time);
 
         ros::NodeHandle& nh_;
 
@@ -65,30 +71,79 @@ velocity_controller_(velocity_controller)
 {
     velocity_targets_subsriber_ = nh_.subscribe("movement_velocity_targets", 100, &AccelerationPlanner::processVelocityCommand, this);
 
-    // Create the dispatch timer but do not call it
-    dispatch_timer_ = nh_.createTimer(ros::Duration(0.0), &AccelerationPlanner<T>::dispatchVelocity, this, true);
-    dispatch_timer_.stop();
+    // Create the dispatch timer
+    dispatch_timer_ = nh_.createTimer(ros::Duration(0.05), &AccelerationPlanner<T>::dispatchVelocity, this);
+
+    // Put an initial time in the buffer
+    TwistStamped zero_velocity;
+    zero_velocity.header.stamp = ros::Time::now();
+    velocity_targets_.emplace_back(zero_velocity);
 }
 
 
 template<class T>
 void AccelerationPlanner<T>::dispatchVelocity(const ros::TimerEvent&)
 {
-    if(velocity_targets_.size() == 0)
+    // Cache time to make sure it stays the same
+    ros::Time current_time = ros::Time::now();
+
+    trimVelocityQueue(current_time);
+
+    TwistStamped target_twist;
+    if(!velocity_targets_.empty())
     {
-        ROS_WARN("Dispatch velocity called with no velocity targets");
+        if(velocity_targets_.size() == 1U)
+        {
+            target_twist = velocity_targets_[0];
+        }
+        else
+        {
+            target_twist = interpolateTwists(velocity_targets_[0], velocity_targets_[1], current_time);
+        }
     }
     else
     {
-        //velocity_controller_.setTargetVelocity(velocity_targets.pop_front());
+        ROS_WARN("No velocity targets");
     }
 
-    // Schedule the next velocity dispatch if it exists
-    if(!velocity_targets_.empty())
+    //velocity_controller_.setTargetVelocity(target_twist);
+}
+
+template<class T>
+TwistStamped AccelerationPlanner<T>::interpolateTwists(TwistStamped& begin, TwistStamped& end, ros::Time time)
+{
+    TwistStamped interpolated;
+    return interpolated;
+}
+
+template<class T>
+void AccelerationPlanner<T>::trimVelocityQueue(ros::Time time)
+{
+    // We want to remove everything before the first time less than followed by a time more than or equal
+
+    if(velocity_targets_.empty())
     {
-        dispatch_timer_.setPeriod(velocity_targets_.front().header.stamp - ros::Time::now());
+        // Nothing to do here
+        return;
+    }
+
+    // Find the first time more than or equal
+    TwistArrayStamped::iterator it = std::lower_bound(velocity_targets_.begin(), velocity_targets_.end(), time,
+                                                      [](auto& twist, auto& time) { return twist.header.stamp < time; });
+    
+
+    if(it == velocity_targets_.begin())
+    {
+        // ALl the times are greater than the current time
+        ROS_ASSERT("trimVelocityQueue there are no valid velocities available");
+    }
+    else
+    {
+        // it is somwhere in the middle go delete stuff
+        (void)velocity_targets_.erase(velocity_targets_.begin(), std::prev(it, 1));
     }
 }
+
 
 // Receive a new list of velocities commands and place them into our list properly
 // Basic Operation:
@@ -117,8 +172,6 @@ void AccelerationPlanner<T>::processVelocityCommand(const iarc7_msgs::TwistStamp
     else if(velocity_targets_.size() == 1U && 
             velocity_targets_.front().header.stamp < static_cast<ros::Time>(message->data.front().header.stamp))
     {
-        // Stop the timer since it's invalid now
-        dispatch_timer_.stop();
         // Remove the first element since it's invalid
         velocity_targets_.erase(velocity_targets_.begin());
     }
@@ -127,21 +180,14 @@ void AccelerationPlanner<T>::processVelocityCommand(const iarc7_msgs::TwistStamp
         ros::Time target_time = static_cast<ros::Time>(message->data.front().header.stamp);
 
         // Get an iterator to the first time more than the first time from the message
-        TwistArrayStamped::iterator it = std::upper_bound(velocity_targets_.begin(), velocity_targets_.end(),
-                                                          target_time,
-                                                          [](auto& a, auto& b) { return a < b.header.stamp; });
-
-        // If its the first item stop the timer since we will need to reschedule it
-        if(it == velocity_targets_.begin())
-        {
-            dispatch_timer_.stop();
-        }
+        TwistArrayStamped::iterator it = std::lower_bound(velocity_targets_.begin(), velocity_targets_.end(), target_time,
+                                                          [](auto& twist, auto& time) { return twist.header.stamp < time; });
 
         // Check if its the last element and decide whether to remove it
         if(it == velocity_targets_.end() && target_time <= it->header.stamp)
         {
             // Remove last element
-            (void)velocity_targets_.pop_back();
+            velocity_targets_.pop_back();
         }
         else
         {
@@ -153,12 +199,6 @@ void AccelerationPlanner<T>::processVelocityCommand(const iarc7_msgs::TwistStamp
 
     // Copy all future targets into our buffer. All targets that were after these targets should be deleted now.
     std::for_each(message->data.begin(), message->data.end(), [&](auto i){ velocity_targets_.emplace_back(i); } );
-
-    // If the dispatch timer wasn't already scheduled make sure to call it
-    if(!dispatch_timer_.hasPending())
-    {
-        dispatch_timer_.setPeriod(velocity_targets_.front().header.stamp - ros::Time::now());
-    }
 }
 
 #endif // ACCLERATION_PLANNER_H
