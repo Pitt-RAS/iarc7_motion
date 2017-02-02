2207574a2622cf259a449990d3a56bac659ec8d2
Levi Burner <leviburner@gmail.com>
2017-02-01 20:13:00 -0500

    Rename use pythonic convention for local variable names

--- scripts/motion_planner.py
+++ scripts/motion_planner.py
@@ -6,32 +6,32 @@ from task_state import TaskState
 
 class MotionPlanner:
 
-    def __init__(self, action_server):
-        self.action_server = action_server
+    def __init__(self, _action_server):
+        self._action_server = _action_server
         self.task = None
 
     def get_velocity_command(self):
-        if (self.task is None) and self.action_server.has_new_task():
-            self.task = self.action_server.get_new_task()
+        if (self.task is None) and self._action_server.has_new_task():
+            self.task = self._action_server.get_new_task()
 
         if self.task:
-            if self.action_server.is_canceled():
+            if self._action_server.is_canceled():
                 print("canceling")
                 self.task.cancel()
 
             (task_state, preferred_twist) = self.task.get_preferred_velocity()
 
             if task_state == TaskState.canceled:
-                self.action_server.set_canceled()
+                self._action_server.set_canceled()
                 self.task = None
             elif task_state == TaskState.aborted:
-                self.action_server.set_aborted()
+                self._action_server.set_aborted()
                 self.task = None
             elif task_state == TaskState.failed:
-                self.action_server.set_succeeded(False)
+                self._action_server.set_succeeded(False)
                 self.task = None
             elif task_state == TaskState.done:
-                self.action_server.set_succeeded(True)
+                self._action_server.set_succeeded(True)
                 self.task = None
             else:
                 assert task_state == TaskState.running
