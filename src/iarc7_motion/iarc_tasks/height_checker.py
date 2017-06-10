96e8027fcb2134567251c44e7baabcde5e4d2a8a
Andrew Saba <asaba96@pitt.edu>
2017-06-10 17:59:31 -0400

    Added check that the desired z height is not too far from the drone's current height

--- src/iarc7_motion/iarc_tasks/height_checker.py
+++ src/iarc7_motion/iarc_tasks/height_checker.py
@@ -16,15 +16,24 @@ class HeightChecker():
         self._lock = threading.RLock()
         try:
             self._MIN_MANEUVER_HEIGHT = rospy.get_param('~min_maneuver_height')
+            self._MAX_Z_ERROR = rospy.get_param('~max_z_error')
         except KeyError as e:
             rospy.logerr('Could not lookup a parameter for track roomba task')
             raise
 
     # uses a p-controller to return a velocity to maintain a height
     # that is set as the param _track_roomba_height
-    def above_min_maneuver_height(self, height):
+    def above_min_maneuver_height(self, current_height):
         with self._lock:
-            if height  < self._MIN_MANEUVER_HEIGHT:
+            if (current_height < self._MIN_MANEUVER_HEIGHT):
+                return False
+            else:
+                return True
+
+    def check_z_error(self, current_height, desired_height):
+        with self._lock:
+            z_error = abs(current_height-desired_height)
+            if (z_error > self._MAX_Z_ERROR):
                 return False
             else:
                 return True
