d9e9c89b95f89acef2d48db839203c747209cbcf
Andrew Saba <asaba96@pitt.edu>
2017-07-11 20:46:15 -0400

    Added params block roomba task Added block roomba to task action server

--- src/iarc7_motion/iarc_task_action_server.py
+++ src/iarc7_motion/iarc_task_action_server.py
@@ -12,6 +12,7 @@ from iarc_tasks.test_task import TestTask
 from iarc_tasks.xyztranslation_task import XYZTranslationTask
 from iarc_tasks.track_roomba_task import TrackRoombaTask
 from iarc_tasks.hit_roomba_task import HitRoombaTask
+from iarc_tasks.block_roomba_task import BlockRoombaTask
 from iarc_tasks.hold_position_task import HoldPositionTask
 
 class IarcTaskActionServer:
@@ -37,6 +38,7 @@ class IarcTaskActionServer:
                            'xyztranslate': XYZTranslationTask,
                            'track_roomba': TrackRoombaTask,
                            'hit_roomba': HitRoombaTask,
+                           'block_roomba': BlockRoombaTask,
                            'hold_position': HoldPositionTask,
                            'test_task': TestTask}
 
